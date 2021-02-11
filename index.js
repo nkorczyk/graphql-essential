bf6434d19b6071d46da06f10578b57dbf04d7058
Norbert Korczyk <norbert_korczyk@epam.com>
2021-02-11 08:17:37 +0100

    GraphQL setup

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('GaphQL');
+  res.send('GraphQL');
 });
+
+const root = { hello: () => 'Hello' };
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+);
 
 app.listen(8080, () => console.log('Running on server port localhost:8080/graphql'));
